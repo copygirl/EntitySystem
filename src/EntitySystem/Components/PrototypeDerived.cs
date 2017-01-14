8625dc127743c0bbd0b06da956abffffa451e783
copygirl <halfnerd.copygirl@gmail.com>
2017-01-14 14:48:46 +0100

    Fix PrototypeDerived.Has returning void

--- src/EntitySystem/Components/PrototypeDerived.cs
+++ src/EntitySystem/Components/PrototypeDerived.cs
@@ -7,7 +7,7 @@ namespace EntitySystem.Components
 	{
 		readonly HashSet<Entity> _derivatives = new HashSet<Entity>();
 		
-		public void Has(Entity entity) => _derivatives.Contains(entity);
+		public bool Has(Entity entity) => _derivatives.Contains(entity);
 		public void Add(Entity entity) => _derivatives.Add(entity);
 		public void Remove(Entity entity) => _derivatives.Remove(entity);
 		public void Clear() => _derivatives.Clear();
